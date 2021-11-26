e6373016152edccb7481defd255682d33ec50497
TwinklyStar <18930143913@163.com>
2021-11-26 10:32:48 +0800

    Unified indent

--- t1.C
+++ t1.C
@@ -10,26 +10,26 @@ using namespace std;
 
 //Make histograms about gmm study
 bool t1::GmmStudy() {
-   
-   for(int nHit=0;nHit<det_n;nHit++) {
-      //only interest at font cap detector
-      if(det_ID[nHit]!=22) {
-         continue;
-      }         
 
-      if(PID_to_Name.find(det_VrtxParticleID[nHit])!=PID_to_Name.end()) {         
-         histSvc->SetProcessTag(PID_to_Name[det_VrtxParticleID[nHit]]);
-      }
-      else {
-         histSvc->SetProcessTag("UnknownPID");
-      }
+    for(int nHit=0;nHit<det_n;nHit++) {
+        //only interest at font cap detector
+        if(det_ID[nHit]!=22) {
+            continue;
+        }
 
-      float angle_degree = TMath::ATan(det_y[nHit]/200.)*180/3.141592653;
+        if(PID_to_Name.find(det_VrtxParticleID[nHit])!=PID_to_Name.end()) {
+            histSvc->SetProcessTag(PID_to_Name[det_VrtxParticleID[nHit]]);
+        }
+        else {
+            histSvc->SetProcessTag("UnknownPID");
+        }
 
-      histSvc->BookFillHist("energy",1000,0,10000,det_kine[nHit]);
-      histSvc->BookFillHist("angle",1000,-90,90,angle_degree);
-   }
-   return true;
+        float angle_degree = TMath::ATan(det_y[nHit]/200.)*180/3.141592653;
+
+        histSvc->BookFillHist("energy",1000,0,10000,det_kine[nHit]);
+        histSvc->BookFillHist("angle",1000,-90,90,angle_degree);
+    }
+    return true;
 }
 
 //Make histograms about irradiation study
@@ -37,100 +37,100 @@ bool t1::GmmStudy() {
 //then continued with a Al plate with 1cm thickness
 bool t1::IrradiationStudy()
 {
-   
-   for(int nHit=0;nHit<det_n;nHit++) {
-      if(PID_to_Name.find(det_VrtxParticleID[nHit])!=PID_to_Name.end()) {         
-         histSvc->SetProcessTag(PID_to_Name[det_VrtxParticleID[nHit]]);
-      }
-      else {
-         histSvc->SetProcessTag("UnknownPID");
-      }
 
-      if(det_VrtxParticleID[nHit]==22) {
-         // cout << "gamma e = " << det_kine[nHit] << endl;
-      }
+    for(int nHit=0;nHit<det_n;nHit++) {
+        if(PID_to_Name.find(det_VrtxParticleID[nHit])!=PID_to_Name.end()) {
+            histSvc->SetProcessTag(PID_to_Name[det_VrtxParticleID[nHit]]);
+        }
+        else {
+            histSvc->SetProcessTag("UnknownPID");
+        }
 
-      //Injected electrons distributions
-      if(det_ID[nHit]==20) {
-         histSvc->SetDetectorTag("Injected");                  
-      }
-      //After Cu plate
-      else if(det_ID[nHit]==21) {
-         histSvc->SetDetectorTag("AfterCu");
-      }
-      //After Al plate
-      else if(det_ID[nHit]==24) {
-         histSvc->SetDetectorTag("AfterAl");
-      }
-      //our scintillator
-      else if(det_ID[nHit]==22) {
-         histSvc->SetDetectorTag("Detector");
-      }
-      else {
-         continue;
-      }
+        if(det_VrtxParticleID[nHit]==22) {
+            // cout << "gamma e = " << det_kine[nHit] << endl;
+        }
 
-      // float angle_degree = TMath::ATan(det_y[nHit]/200.)*180/3.141592653;
+        //Injected electrons distributions
+        if(det_ID[nHit]==20) {
+            histSvc->SetDetectorTag("Injected");
+        }
+            //After Cu plate
+        else if(det_ID[nHit]==21) {
+            histSvc->SetDetectorTag("AfterCu");
+        }
+            //After Al plate
+        else if(det_ID[nHit]==24) {
+            histSvc->SetDetectorTag("AfterAl");
+        }
+            //our scintillator
+        else if(det_ID[nHit]==22) {
+            histSvc->SetDetectorTag("Detector");
+        }
+        else {
+            continue;
+        }
 
-      histSvc->BookFillHist("energy",1000,0,500,det_kine[nHit]);
-      histSvc->BookFillHist("edep",1000,0,500,det_edep[nHit]);
-      // histSvc->BookFillHist("angle",1000,-90,90,angle_degree);
-      histSvc->BookFillHist("xy",100,-20,20,100,-20,20,det_x[nHit],det_y[nHit]);      
-   }
-   return true;
+        // float angle_degree = TMath::ATan(det_y[nHit]/200.)*180/3.141592653;
+
+        histSvc->BookFillHist("energy",1000,0,500,det_kine[nHit]);
+        histSvc->BookFillHist("edep",1000,0,500,det_edep[nHit]);
+        // histSvc->BookFillHist("angle",1000,-90,90,angle_degree);
+        histSvc->BookFillHist("xy",100,-20,20,100,-20,20,det_x[nHit],det_y[nHit]);
+    }
+    return true;
 }
 
 
 //Gmumu study based on real geometry
 bool t1::RealGeometryGmm()
 {
-   //mu_p and mu_m share a same vertex, so only draw once per event
-   // bool vertexKineDraw = false; 
-   for(int nHit=0;nHit<det_n;nHit++) {
+    //mu_p and mu_m share a same vertex, so only draw once per event
+    // bool vertexKineDraw = false;
+    for(int nHit=0;nHit<det_n;nHit++) {
 
-      if(PID_to_Name.find(det_VrtxParticleID[nHit])!=PID_to_Name.end()) {
-         histSvc->SetProcessTag(PID_to_Name[det_VrtxParticleID[nHit]]);
-      }
-      else {
-         histSvc->SetProcessTag("UnknownPID");
-      }
+        if(PID_to_Name.find(det_VrtxParticleID[nHit])!=PID_to_Name.end()) {
+            histSvc->SetProcessTag(PID_to_Name[det_VrtxParticleID[nHit]]);
+        }
+        else {
+            histSvc->SetProcessTag("UnknownPID");
+        }
 
-      //DRZ1 monitering the injected electrons
-      if(det_ID[nHit]==4) {
-         histSvc->SetDetectorTag("DRZ1");
-      }
+        //DRZ1 monitering the injected electrons
+        if(det_ID[nHit]==4) {
+            histSvc->SetDetectorTag("DRZ1");
+        }
 
-      //DRZ2 monitering the electron beams after magnets
-      else if(det_ID[nHit]==7) {
-         histSvc->SetDetectorTag("DRZ2");
-      }
-      //Out scintillator
-      else if(det_ID[nHit]==10) {
-         histSvc->SetDetectorTag("Detector");
-      }
-      else {
-         continue;
-      }
+            //DRZ2 monitering the electron beams after magnets
+        else if(det_ID[nHit]==7) {
+            histSvc->SetDetectorTag("DRZ2");
+        }
+            //Out scintillator
+        else if(det_ID[nHit]==10) {
+            histSvc->SetDetectorTag("Detector");
+        }
+        else {
+            continue;
+        }
 
-      // float angle_degree = TMath::ATan(det_y[nHit]/200.)*180/3.141592653;
+        // float angle_degree = TMath::ATan(det_y[nHit]/200.)*180/3.141592653;
 
-      histSvc->BookFillHist("energy",1000,0,500,det_kine[nHit]);
-      histSvc->BookFillHist("edep",1000,0,500,det_edep[nHit]);      
-      histSvc->BookFillHist("xy",1000,-300,300,1000,-200,200,det_x[nHit],det_y[nHit]);
+        histSvc->BookFillHist("energy",1000,0,500,det_kine[nHit]);
+        histSvc->BookFillHist("edep",1000,0,500,det_edep[nHit]);
+        histSvc->BookFillHist("xy",1000,-300,300,1000,-200,200,det_x[nHit],det_y[nHit]);
 
 
-      if(ProcessIDMapping.find(det_VrtxProcID[nHit])!=ProcessIDMapping.end()) {
-         histSvc->SetProdTag(ProcessIDMapping[det_VrtxProcID[nHit]]);
-      }
-      else {
-         histSvc->SetProdTag("UnknownProd");  
-      }
-      // histSvc->SetProcessTag("");
-      histSvc->BookFillHist("energy",1000,0,500,det_kine[nHit]);
-      histSvc->BookFillHist("edep",1000,0,500,det_edep[nHit]);
-      histSvc->BookFillHist("xy",1000,-300,300,1000,-200,200,det_x[nHit],det_y[nHit]);      
-   }
-   return true;
+        if(ProcessIDMapping.find(det_VrtxProcID[nHit])!=ProcessIDMapping.end()) {
+            histSvc->SetProdTag(ProcessIDMapping[det_VrtxProcID[nHit]]);
+        }
+        else {
+            histSvc->SetProdTag("UnknownProd");
+        }
+        // histSvc->SetProcessTag("");
+        histSvc->BookFillHist("energy",1000,0,500,det_kine[nHit]);
+        histSvc->BookFillHist("edep",1000,0,500,det_edep[nHit]);
+        histSvc->BookFillHist("xy",1000,-300,300,1000,-200,200,det_x[nHit],det_y[nHit]);
+    }
+    return true;
 }
 
 //Make histograms about irradiation study
@@ -139,73 +139,73 @@ bool t1::RealGeometryGmm()
 //
 bool t1::RealGeometryIrradiation()
 {
-   
-   for(int nHit=0;nHit<det_n;nHit++) {
-      if(PID_to_Name.find(det_VrtxParticleID[nHit])!=PID_to_Name.end()) {         
-         histSvc->SetProcessTag(PID_to_Name[det_VrtxParticleID[nHit]]);
-      }
-      else {
-         histSvc->SetProcessTag("UnknownPID");
-      }
 
-      
+    for(int nHit=0;nHit<det_n;nHit++) {
+        if(PID_to_Name.find(det_VrtxParticleID[nHit])!=PID_to_Name.end()) {
+            histSvc->SetProcessTag(PID_to_Name[det_VrtxParticleID[nHit]]);
+        }
+        else {
+            histSvc->SetProcessTag("UnknownPID");
+        }
 
-      //DRZ1 monitering the injected electrons
-      if(det_ID[nHit]==4) {
-         histSvc->SetDetectorTag("DRZ1");
-      }
-      //DRZ2 monitering the electron beams after magnets
-      else if(det_ID[nHit]==7) {
-         histSvc->SetDetectorTag("DRZ2");
-      }
-      //Out scintillator
-      else if(det_ID[nHit]==9) {
-         histSvc->SetDetectorTag("Detector");
-      }
-      else {
-         continue;
-      }
 
-      // float angle_degree = TMath::ATan(det_y[nHit]/200.)*180/3.141592653;
 
-      histSvc->BookFillHist("energy",1000,0,500,det_kine[nHit]);
-      histSvc->BookFillHist("edep",1000,0,500,det_edep[nHit]);      
-      histSvc->BookFillHist("xy",1000,-300,300,1000,-200,200,det_x[nHit],det_y[nHit]);
+        //DRZ1 monitering the injected electrons
+        if(det_ID[nHit]==4) {
+            histSvc->SetDetectorTag("DRZ1");
+        }
+            //DRZ2 monitering the electron beams after magnets
+        else if(det_ID[nHit]==7) {
+            histSvc->SetDetectorTag("DRZ2");
+        }
+            //Out scintillator
+        else if(det_ID[nHit]==9) {
+            histSvc->SetDetectorTag("Detector");
+        }
+        else {
+            continue;
+        }
 
-      if(ProcessIDMapping.find(det_VrtxProcID[nHit])!=ProcessIDMapping.end()) {
-         histSvc->SetProdTag(ProcessIDMapping[det_VrtxProcID[nHit]]);
-      }
-      else {
-         histSvc->SetProdTag("UnknownProd");  
-      }
-      histSvc->SetProcessTag("");
-      histSvc->BookFillHist("energy",1000,0,500,det_kine[nHit]);
-      histSvc->BookFillHist("edep",1000,0,500,det_edep[nHit]);
-      histSvc->BookFillHist("xy",1000,-300,300,1000,-200,200,det_x[nHit],det_y[nHit]);
-   }
-   return true;
+        // float angle_degree = TMath::ATan(det_y[nHit]/200.)*180/3.141592653;
+
+        histSvc->BookFillHist("energy",1000,0,500,det_kine[nHit]);
+        histSvc->BookFillHist("edep",1000,0,500,det_edep[nHit]);
+        histSvc->BookFillHist("xy",1000,-300,300,1000,-200,200,det_x[nHit],det_y[nHit]);
+
+        if(ProcessIDMapping.find(det_VrtxProcID[nHit])!=ProcessIDMapping.end()) {
+            histSvc->SetProdTag(ProcessIDMapping[det_VrtxProcID[nHit]]);
+        }
+        else {
+            histSvc->SetProdTag("UnknownProd");
+        }
+        histSvc->SetProcessTag("");
+        histSvc->BookFillHist("energy",1000,0,500,det_kine[nHit]);
+        histSvc->BookFillHist("edep",1000,0,500,det_edep[nHit]);
+        histSvc->BookFillHist("xy",1000,-300,300,1000,-200,200,det_x[nHit],det_y[nHit]);
+    }
+    return true;
 }
 
 void t1::Loop()
 {
-   if (fChain == 0) return;
+    if (fChain == 0) return;
 
-   Long64_t nentries = fChain->GetEntriesFast();
+    Long64_t nentries = fChain->GetEntriesFast();
 
-   Long64_t nbytes = 0, nb = 0;
+    Long64_t nbytes = 0, nb = 0;
 
-   for (Long64_t jentry=0; jentry<nentries;jentry++) {
-      Long64_t ientry = LoadTree(jentry);
-      if (ientry < 0) break;
+    for (Long64_t jentry=0; jentry<nentries;jentry++) {
+        Long64_t ientry = LoadTree(jentry);
+        if (ientry < 0) break;
 
-      if(jentry%100000==0) {
-         cout << "processed " << jentry << "events" << endl;
-      }
+        if(jentry%100000==0) {
+            cout << "processed " << jentry << "events" << endl;
+        }
 
-      nb = fChain->GetEntry(jentry);   nbytes += nb;
+        nb = fChain->GetEntry(jentry);   nbytes += nb;
 
-      histSvc->InitNameSvc();      
-      (this->*func_anlysis_method)();
-   }
-   histSvc->Write();
+        histSvc->InitNameSvc();
+        (this->*func_anlysis_method)();
+    }
+    histSvc->Write();
 }
